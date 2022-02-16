cf2aeeeef5ff38ab32c21ef4826a78958605d7bf
Chenyang Zhong <zhongcy95@gmail.com>
2022-02-16 04:38:45 -0500

    nx563j: Specify androidboot.boot_devices
    So that /dev/block/by-name/ symlinks are created.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: I270c1899e4487ed5e7657038c4ed07e407476953

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,6 +67,7 @@ BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom \
 	swiotlb=2048 \
 	loop.max_part=7 \
         androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE += androidboot.boot_devices=soc/1da4000.ufshc
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
