e640bf7675df742326ea85ea864ace94ecfb92c6
Vladimir Mikhailov <beykerykt@gmail.com>
2024-11-30 01:11:43 +0900

    nx563j: Tree cleanup

--- twrp_nx563j.mk
+++ twrp_nx563j.mk
@@ -17,21 +17,12 @@
 # Release name
 PRODUCT_RELEASE_NAME := nx563j
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Inherit AOSP product makefiles
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
-
-# qcom standard decryption
-PRODUCT_PACKAGES += \
-    qcom_decrypt \
-    qcom_decrypt_fbe
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hardware.keystore=msm8998 \
-    ro.hardware.gatekeeper=msm8998
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := nx563j
@@ -41,12 +32,6 @@ PRODUCT_MODEL := Z17
 PRODUCT_MANUFACTURER := nubia
 
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=NX563J PRODUCT_NAME=NX563J
-
-# Verity
-PRODUCT_SUPPORTS_BOOT_SIGNER := true
-PRODUCT_SUPPORTS_VERITY := false
-PRODUCT_SUPPORTS_VERITY_FEC := false
-PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/verity
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="NX563J-user 7.1.1 NMF26X eng.nubia.20181008.160254 release-keys"
@@ -62,3 +47,18 @@ PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     fastbootd
 endif
+
+# qcom standard decryption
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=msm8998 \
+    ro.hardware.gatekeeper=msm8998
+
+# Verity
+PRODUCT_SUPPORTS_BOOT_SIGNER := true
+PRODUCT_SUPPORTS_VERITY := false
+PRODUCT_SUPPORTS_VERITY_FEC := false
+PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/verity
